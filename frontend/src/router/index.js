ced99069356bf9044e0005e5057a0b9a2b7cb8f4
Julien FERAND <julien.ferand@gmail.com>
2020-10-14 12:23:33 +0200

    login et signup placeholders

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,7 +15,12 @@ const routes = [{
     path: '/login',
     name: 'Login',
     component: () => import( /* webpackChunkName: "login" */ '../views/Login.vue')
-  }
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import( /* webpackChunkName: "signup" */ '../views/Signup.vue')
+  },
 ]
 
 const router = new VueRouter({
@@ -26,7 +31,7 @@ router.beforeEach(async (to, from, next) => {
   // Checks if user is valid, else directs to login page (user can still go to signup)
   if (!(to.path === '/login' || to.path === '/signup')) {
     const userLocalStorage = userLocalStorageFetch();
-    await store.dispatch('setUser', userLocalStorage);
+    await store.dispatch('setUser', userLocalStorage);  // Surement pas idéal mais ça va le faire
     if(store.state.currentUser.logValid){
       next();
     }
